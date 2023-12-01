5194b4a1a766de0a97bdc621c217c65fd7746bcc
Monika25092003 <152387700+Monika25092003@users.noreply.github.com>
2023-12-01 14:32:57 +0530

    Python task 1 and 2

---
+++ main.py
@@ -0,0 +1,14 @@
+
+#task 1
+print("Hello World")
+
+#task 2
+name="Monika"
+number=634
+number1=43.5
+print(name,type(name))
+print(id(name))
+print(number,type(number))
+print(id(number))
+print(number1,type(number1))
+print(id(number1))
